ed1f794b30d7b4dead3aebfeb1a86d67b692fe01
José Chavarría <jchavarria@momotolabs.com>
2024-10-02 18:20:39 -0600

    base and auth

---
+++ front/src/config/config.ts
@@ -0,0 +1,5 @@
+import {environment} from "../environments/environment";
+
+export const URL_API = environment.URL_API
+export const URL_BACKEND = environment.URL_BACKEND
+export const URL_FRONTEND = environment.URL_FRONTEND
